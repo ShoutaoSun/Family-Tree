d902c8383c7ee8edf972db7dc7eecfa3205fdc38
ShoutaoSun <sunshoutao0827@163.com>
2026-04-09 16:29:27 +0800

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -5,4 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/Family-Tree/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
 })
