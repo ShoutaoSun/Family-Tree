b3061928e15d7785be412a3606fbd3d6457603d5
ShoutaoSun <sunshoutao0827@163.com>
2026-04-10 12:36:57 +0800

    Updates

---
+++ assets/AboutView-DfuPOFjt.js
@@ -0,0 +1,1 @@
+import{a as e,i as t,m as n,t as r}from"./_plugin-vue_export-helper-X7zpvrUa.js";var i={},a={class:`about`};function o(r,i){return n(),e(`div`,a,[...i[0]||=[t(`h1`,null,`This is an about page`,-1)]])}var s=r(i,[[`render`,o]]);export{s as default};
